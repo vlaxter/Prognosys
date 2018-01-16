8280481cfa2d8dda725b827ed7a57c94f57f5f7d
Vladimir Rivera <vladimir.rivera@outlook.com>
2018-01-15 23:46:36 -0500

    Basic end2end completed

--- Prognosys.Web/App_Start/RouteConfig.cs
+++ Prognosys.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Prognosys.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Clients", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Prognosys.Web/Controllers/ClientsController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using Prognosys.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prognosys.Web.Controllers
+{
+    public class ClientsController : Controller
+    {
+        string baseUrl = "http://prognosys.api.local/v1/";
+
+        public async Task<ActionResult> Index()
+        {
+            List<ClientModel> EmpInfo = new List<ClientModel>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url  
+                client.BaseAddress = new Uri(baseUrl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("Clients");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    EmpInfo = JsonConvert.DeserializeObject<List<ClientModel>>(EmpResponse);
+
+                }
+                //returning the employee list to view  
+                return View(EmpInfo);
+            }
+        }
+    }
+}
