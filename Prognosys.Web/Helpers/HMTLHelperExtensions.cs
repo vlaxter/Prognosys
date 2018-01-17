41ecf1d2ebcd40758fb42a4c2b9ff5ae9e9ac0b1
Vladimir Rivera <vladimir.rivera@outlook.com>
2018-01-16 22:47:47 -0500

    Ui refactored with inspinia using OWIN

---
+++ Prognosys.Web/Helpers/HMTLHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Prognosys.Web
+{
+    public static class HMTLHelperExtensions
+    {
+        public static string IsSelected(this HtmlHelper html, string controller = null, string action = null)
+        {
+            string cssClass = "active";
+            string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            string currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (String.IsNullOrEmpty(controller))
+                controller = currentController;
+
+            if (String.IsNullOrEmpty(action))
+                action = currentAction;
+
+            return controller == currentController && action == currentAction ?
+                cssClass : String.Empty;
+        }
+    }
+}
---
+++ Prognosys.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Prognosys.Web.Startup))]
+namespace Prognosys.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
